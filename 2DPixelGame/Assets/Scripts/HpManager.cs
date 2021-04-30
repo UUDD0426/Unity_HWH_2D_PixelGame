eb5fff3b0c2d53763599bb984453d49c8eb7eef7
unknown <71476539+UUDD0426@users.noreply.github.com>
2021-04-30 09:47:29 +0800

    0430

---
+++ 2DPixelGame/Assets/Scripts/HpManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HpManager : MonoBehaviour
+{
+    [Header("血條")]
+    public Image bar;
+
+    /// <summary>
+    /// 輸入血量與血量最大值並更新血條
+    /// </summary>
+    /// <param name="hp">當前血量</param>
+    /// <param name="hpBar">血量最大值</param>
+    public void UpdateHpBar(float hp,float hpMax)
+    {
+        //血條.填滿數值=當前血量/血量最大值
+        bar.fillAmount = hp / hpMax;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Unity_HWH_2D_PixelGame
- Unity 華夏科大2D 像素類型遊戲_守望傳說
+ Unity 華夏科大 2D 像素類型遊戲-守望傳說
